5126ea0fd834d569825bce699d8ebff0a723e0d5
antoinechauvn <antoine.chauvin@live.fr>
2023-08-30 15:08:06 +0200

    Add Details pages and CustomButton Components

---
+++ components/CustomButton.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import { Text, StyleSheet, Pressable } from 'react-native';
+
+const CustomButton = (props) => {
+  const { onPress, title = 'Default text' } = props;
+  
+  return (
+    <Pressable style={styles.button} onPress={onPress}>
+      <Text style={styles.text}>{title}</Text>
+    </Pressable>
+  );
+};
+
+const styles = StyleSheet.create({
+  button: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    paddingVertical: 12,
+    paddingHorizontal: 32,
+    borderRadius: 4,
+    elevation: 3,
+    backgroundColor: 'black',
+  },
+  text: {
+    fontSize: 16,
+    lineHeight: 21,
+    fontWeight: 'bold',
+    letterSpacing: 0.25,
+    color: 'white',
+  },
+});
+
+export default CustomButton;
--- components/Details.js
+++ components/Details.js
@@ -1,27 +1,117 @@
 import React from 'react';
-import { View, Text, Button, StyleSheet, SafeAreaView } from 'react-native';
+import { View, Text, Button, StyleSheet, SafeAreaView, Image} from 'react-native';
+import {  useFonts, Montserrat_700Bold, Montserrat_500Medium } from '@expo-google-fonts/montserrat';
+import AppLoading from 'expo-app-loading';
+import CustomButton from './CustomButton';
 
 export default function Details({ navigation }) {
-    return (
-        <View style={styles.container}>
-          <SafeAreaView>
-            <View style={styles.headerWrapper}>
-              <View style={styles.headerLeft}>
-                
-              </View>
+  let [fontsLoaded] = useFonts({
+    Montserrat_700Bold,
+    Montserrat_500Medium,
+  });
 
+  if (!fontsLoaded) {
+    return <AppLoading />;
+  }
+
+    return (
+      <View style={styles.container}>
+        <SafeAreaView>
+          <View style={styles.headerWrapper}>
+
+          </View>
+        </SafeAreaView>
+
+        <View style={styles.titlesWrapper}>
+          <Text style={styles.title}>
+            Nom du Plat
+          </Text>
+        </View>
+
+        <View style={styles.priceWrapper}>
+          <Text style={styles.priceText}>1000€</Text>
+        </View>
+
+        <View style={styles.infoWrapper}>
+          <View style={styles.infoLeftWrapper}>
+            <View style={styles.infoItemWrapper}>
+              <Text style={styles.infoItemTitle}>Categorie</Text>
+              <Text style={styles.infoItemText}>Pizza</Text>
             </View>
 
-          </SafeAreaView>
+            <View style={styles.infoItemWrapper}>
+              <Text style={styles.infoItemTitle}>Description</Text>
+              <Text style={styles.infoItemText}>Ceci est une pizza</Text>
+            </View>
+
+
+            <View style={styles.infoItemWrapper}>
+              <Text style={styles.infoItemTitle}>Allergène</Text>
+              <Text style={styles.infoItemText}>Non</Text>
+            </View>
+          </View>
+          <View>
+            <Image source={require('../assets/pizza1.png')} style={styles.itemPicture} />
+          </View>
         </View>
+        <CustomButton title='Acheter' onPress={() => console.log('Bouton Enregistrer pressé')}>
+        </CustomButton>
+      </View>
       );
 }
 
 const styles = new StyleSheet.create({
   container: {
     flex: 1,
-  }
-}
+  },
 
+  titlesWrapper: {
+    paddingHorizontal: 20,
+    marginTop: 30,
+  },
+
+  title: {
+    fontFamily: 'Montserrat_700Bold',
+    fontSize: 32,
+  },
+  priceWrapper: {
+    marginTop: 20,
+    paddingHorizontal: 20,
+  },
+  priceText: {
+    fontFamily: 'Montserrat_700Bold',
+    fontSize: 32,
+    color: "#e28743"
+  },
+  infoWrapper: {
+    marginTop: 60,
+    flexDirection: 'row',
+    justifyContent: 'flex-start',
+    alignItems: 'center'
+  },
+  infoLeftWrapper: {
+    paddingLeft: 20,
+
+  },
+  infoItemWrapper: {
+    marginBottom: 20,
+  },
+  infoItemTitle: {
+    fontFamily: 'Montserrat_500Medium',
+    fontSize: 16,
+    color:'#BBB5BD',
+  },
+  infoItemText: {
+    fontFamily: 'Montserrat_500Medium',
+    fontSize: 20,
+  },
+  itemPicture: {
+    marginLeft: 30,
+    width: 230, 
+    height: 230,
+    resizeMode: 'contain',
+  }
+
+}
 )
 
