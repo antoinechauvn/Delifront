fcb17ab9d5e35b5929108cb1dd566e7223b49217
antoinechauvn <antoine.chauvin@live.fr>
2023-09-01 15:58:18 +0200

    Fix Articles and Home

--- pages/Articles.js
+++ pages/Articles.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View } from 'react-native';
+import { View,ScrollView, SafeAreaView } from 'react-native';
 import AppHeader from '../components/AppHeader';
 import { Searchbar } from 'react-native-paper';
 import ItemArticle from '../components/ItemArticle';
@@ -10,19 +10,21 @@ export default function Articles({ navigation }) {
     const onChangeSearch = query => setSearchQuery(query);
 
     return (
-    <View style={{ flex: 1, backgroundColor: 'white', }}>
+    <View style={{ flex: 1, backgroundColor: 'white'}}>
         <AppHeader title="Articles" navigation={navigation} />
         <Searchbar placeholder="Search" onChangeText={onChangeSearch} value={searchQuery} />
         {/* Contenu principal */}
-        <View style={{ flex: 1 }}>
-            {/* Liste des Articles */}
-            <View style={{ marginTop: 20 }}>
-                <ItemArticle title="Oeuf Mimosa" imagepath={require('../assets/entrees/oeuf.png')} onPress={() => { navigation.navigate('Details');}}></ItemArticle>
+        <ScrollView style={{backgroundColor: 'white'}}>
+            <View style={{marginTop: 20, paddingBottom: 50}}>
+                <ItemArticle title="Oeuf Mimosa" imagepath={require('../assets/entrees/oeuf.png')} onPress={() => { navigation.navigate('Articles');}}></ItemArticle>
+                <ItemArticle title="Oeuf Mimosa" imagepath={require('../assets/entrees/oeuf.png')} onPress={() => { navigation.navigate('Articles');}}></ItemArticle>
+                <ItemArticle title="Oeuf Mimosa" imagepath={require('../assets/entrees/oeuf.png')} onPress={() => { navigation.navigate('Articles');}}></ItemArticle>
+                <ItemArticle title="Oeuf Mimosa" imagepath={require('../assets/entrees/oeuf.png')} onPress={() => { navigation.navigate('Articles');}}></ItemArticle>
                 <ItemArticle title="Oeuf Mimosa" imagepath={require('../assets/entrees/oeuf.png')} onPress={() => { navigation.navigate('Articles');}}></ItemArticle>
                 <ItemArticle title="Oeuf Mimosa" imagepath={require('../assets/entrees/oeuf.png')} onPress={() => { navigation.navigate('Articles');}}></ItemArticle>
                 <ItemArticle title="Oeuf Mimosa" imagepath={require('../assets/entrees/oeuf.png')} onPress={() => { navigation.navigate('Articles');}}></ItemArticle>
             </View>
-        </View>
+        </ScrollView>
     </View>
     );
 }
--- pages/Home.js
+++ pages/Home.js
@@ -1,29 +1,31 @@
 import React from 'react';
-import { View } from 'react-native';
+import { View, ScrollView } from 'react-native';
 import AppHeader from '../components/AppHeader';
 import { Searchbar } from 'react-native-paper';
 import ItemCategory from '../components/ItemCategory';
+import { SafeAreaView } from 'react-native-safe-area-context';
 
 export default function Home({ navigation }) {
 
     const [searchQuery, setSearchQuery] = React.useState('');
     const onChangeSearch = query => setSearchQuery(query);
+    const categories = ["Entree","Plat","Dessert","Soupe","Fromage"];
 
     return (
     <View style={{ flex: 1, backgroundColor: 'white', }}>
         <AppHeader title="TestHome" navigation={navigation} />
         <Searchbar placeholder="Search" onChangeText={onChangeSearch} value={searchQuery} />
         {/* Contenu principal */}
-        <View style={{ flex: 1 }}>
+        <ScrollView>
             {/* Liste des catégories */}
-            <View>
-                <ItemCategory title="Entrée" imagepath={require('../assets/categories/entree.jpg')} onPress={() => { navigation.navigate('Articles');}}></ItemCategory>
-                <ItemCategory title="Plats" imagepath={require('../assets/categories/plat.jpg')} onPress={() => { alert('Catégorie cliquée !');}}></ItemCategory>
-                <ItemCategory title="Desserts" imagepath={require('../assets/categories/dessert.png')} onPress={() => { alert('Catégorie cliquée !');}}></ItemCategory>
-                <ItemCategory title="Soupes" imagepath={require('../assets/categories/soupe.jpg')} onPress={() => { alert('Catégorie cliquée !');}}></ItemCategory>
-                <ItemCategory title="Fromage" imagepath={require('../assets/categories/fromage.jpg')} onPress={() => { alert('Catégorie cliquée !');}}></ItemCategory>
-            </View>
-        </View>
+            <SafeAreaView>
+                <ItemCategory title={categories[0]} imagepath={require('../assets/categories/entree.jpg')} onPress={() => { navigation.navigate('Articles');}}></ItemCategory>
+                <ItemCategory title={categories[1]} imagepath={require('../assets/categories/plat.jpg')} onPress={(title) => { alert("Catégorie "+categories[1]+" cliquée !");}}></ItemCategory>
+                <ItemCategory title={categories[2]} imagepath={require('../assets/categories/dessert.png')} onPress={() => { alert('Catégorie cliquée !');}}></ItemCategory>
+                <ItemCategory title={categories[3]} imagepath={require('../assets/categories/soupe.jpg')} onPress={() => { alert('Catégorie cliquée !');}}></ItemCategory>
+                <ItemCategory title={categories[4]} imagepath={require('../assets/categories/fromage.jpg')} onPress={() => { alert('Catégorie cliquée !');}}></ItemCategory>
+            </SafeAreaView>
+        </ScrollView>
     </View>
     );
 }
